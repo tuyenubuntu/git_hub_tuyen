2220e64c2e9b8cd7fa04edfa58db2fa66b8d9313
Tuyen <tuyen.tuyentruongthanh@gmail.com>
2023-07-20 20:43:38 +0700

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,18 @@
+mkdir app
+cd app
+mkdir blueprints static templates
+touch app.py .gitignore
+cd blueprints
+touch __init__.py
+mkdir home_page
+cd home_page
+touch __init__.py blueprint.py
+cd ..
+cd ..
+cd static
+touch styles.css index.js
+cd ..
+cd templates
+touch base.html home.html
+cd ..
+cd ..
